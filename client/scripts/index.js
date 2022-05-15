bc4f1d1f2a1795d2ee0a8ee9024e8a9d3357bf1f
xhmyjae <tomatis.mt@gmail.com>
2022-05-15 16:52:18 +0200

    feat: animation done

---
+++ client/scripts/index.js
@@ -0,0 +1,3 @@
+TweenMax.to('.seq-a',5,{repeat:-1,backgroundPosition:"0px -46680px",ease:SteppedEase.config(120)});
+TweenMax.to('.seq-b',5,{repeat:-1,backgroundPosition:"0px -39864px",ease:SteppedEase.config(132)});
+
