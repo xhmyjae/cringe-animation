51751e2b5ae3fb85515f386fef69cf72e52671c3
xhmyjae <tomatis.mt@gmail.com>
2022-06-07 00:22:34 +0200

    feat: refactor files + add docs

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
+/* Animating the background position of the two elements with gsap */
 TweenMax.to('.seq-a',5,{repeat:-1,backgroundPosition:"0px -46680px",ease:SteppedEase.config(120)});
 TweenMax.to('.seq-b',5,{repeat:-1,backgroundPosition:"0px -39864px",ease:SteppedEase.config(132)});
-
